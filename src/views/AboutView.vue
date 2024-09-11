4e107791ce0c98141df93b1533a92a03a3c73e05
nomomax <nomomax@gmail.com>
2024-09-11 21:08:42 +0800

    add env

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,19 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_API, import.meta.env.VITE_PATH)
+  }
+}
+</script>
+
 <style>
 @media (min-width: 1024px) {
   .about {
