b981a545333c4db68c826fe52918a6b4a52b7fb5
nomomax <nomomax@gmail.com>
2024-09-23 23:10:29 +0800

    add filters function

--- src/main.js
+++ src/main.js
@@ -8,12 +8,16 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import Loading from 'vue3-loading-overlay'
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 
-import Loading from 'vue3-loading-overlay';
-import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
+import { currency } from '@/methods/filters'
 
 const app = createApp(App)
 
+app.config.globalProperties.$filters = {
+  currency
+}
 app.use(createPinia())
 app.use(VueAxios, axios)
 app.use(router)
